f807bf2279d4a2fa942defaac2f8dba504817a9d
LeonardoLV19 <128534196+LeonardoLV19@users.noreply.github.com>
2025-04-21 20:58:17 -0600

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,43 @@
 'use client';
 
 import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import clsx from "clsx"
+
+const link = [
+    {href : '/top-rated', label: 'Top Rated'},
+    {href : '/popular', label: 'Popular'},
+    {href : '/my-favorites', label: 'My Favorites'},
+    {href : '/now-playing', label: 'Now Playing'},
+]
 
 const Header = () => {
+    const pathname = usePathname()
+
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full border-b shadow-sm">
+        <div className='container mx-auto flex items-center justify-between px-4 py-3'>
+            <Link 
+            href="/" 
+            className="text-xl font-bold text-gray-800 hover:text-blue-600 transition-colors"
+            >
+            Movies DB
+            </Link>
+            <nav className="flex gap-6">
+                {link.map(({href,label}) => (
+                    <Link 
+                    key={href} 
+                    href={href} 
+                    className={clsx("text-sm font-medium transition-colors  hover:text-blue-600")}
+                    >
+                        {label}
+                    </Link>
+                ))}
+            </nav>
+        </div>
+    </header>
+  );
+};
 
 export default Header
